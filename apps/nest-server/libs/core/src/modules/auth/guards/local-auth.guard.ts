73d62a41e9a2006809876dcc4aa1ad4d4141891e
fruitsdrink <fruitsdrink@126.com>
2024-11-02 12:47:11 +0800

    perf: 优化登录接口

--- apps/nest-server/libs/core/src/modules/auth/guards/local-auth.guard.ts
+++ apps/nest-server/libs/core/src/modules/auth/guards/local-auth.guard.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, type ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import type { LocalStrategy } from '../strategies';
+import { Observable } from 'rxjs';
 
 /**
  * 本地验证守卫
  */
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    return super.canActivate(context);
+  }
+}
